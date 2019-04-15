4e36b44a1ddfdf0aa63e18235f74d63b7cb3fa5b
Jerry <hoffmann.jerry88@gmail.com>
2019-04-15 15:13:00 +0200

    Added DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class DefaultController
+{
+    /**
+     * @Route("/", name="homepage")
+     * @return Response
+     */
+    public function hompageAction(Environment $twig)
+    {
+        return new Response(
+            $twig->render(
+                'Default/homepage.html.twig'
+            )
+        );
+    }
+}
